d6032d1613fe23f3e5f18ab1ad57bcee09a6bc34
Hamdan Esmail <104253507+Hamdan-1@users.noreply.github.com>
2025-03-25 23:03:24 +0400

    Add Compose Compiler Gradle plugin
    Add Compose Compiler Gradle plugin to the project.
    
    * **gradle/libs.versions.toml**
      - Add `compose-compiler = { id = "org.jetbrains.kotlin.plugin.compose", version.ref = "kotlin" }` in the plugins section.
    
    * **build.gradle.kts**
      - Add `alias(libs.plugins.compose.compiler) apply false` in the plugins section.
    
    * **app/build.gradle.kts**
      - Add `alias(libs.plugins.compose.compiler)` in the plugins section.
      - Add `composeCompiler { reportsDestination = layout.buildDirectory.dir("compose_compiler") stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf") }` at the top level.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -38,6 +39,12 @@ android {
         compose = true
     }
 }
+
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
+}
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
